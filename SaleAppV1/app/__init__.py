7f1a5066afb70a8ef012a9b8b6cc83cc4a66d2ba
tonhanlk7103@gmail.com <tonhanlk7103@gmail.com>
2023-11-21 12:47:14 +0700

    update

--- SaleAppV1/app/__init__.py
+++ SaleAppV1/app/__init__.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 app.secret_key = '%#%$#%$#G#$GGEG%H$%H%$H^HTH$Hdgdgdfgfd'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:%s@localhost/saledb' % quote('Admin@123')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SQLALCHEMY_RECORD_QUERIES"] = True
+app.config['PAGE_SIZE'] = 6
 
 db = SQLAlchemy(app)
 login = LoginManager(app=app)
--- SaleAppV1/app/index.py
+++ SaleAppV1/app/index.py
@@ -1,23 +1,25 @@
-from flask import render_template, request, redirect
-from flask_login import login_user
+import math
 
+from flask import render_template, request, redirect
 import dao
 from app import app, login
+from flask_login import login_user
 
 
 @app.route('/')
 def index():
     kw = request.args.get('kw')
+    cate_id = request.args.get('cate_id')
+    page = request.args.get("page")
 
     cates = dao.load_categories()
-    prod = dao.load_products(kw=kw)
+    products = dao.load_products(kw=kw, cate_id=cate_id, page=page)
 
-    return render_template('index.html', categories=cates, products=prod)
+    total = dao.count_product()
 
-
-@login.user_loader
-def load_user(user_id):
-    return dao.get_user_by_id(user_id)
+    return render_template('index.html', categories=cates,
+                           products=products,
+                           pages=math.ceil(total/app.config['PAGE_SIZE']))
 
 
 @app.route('/products/<id>')
@@ -27,21 +29,21 @@ def details(id):
 
 @app.route('/admin/login', methods=['post'])
 def login_admin_process():
-    if request.method == "POST":
-        username = request.form.get('username')
-        password = request.form.get('password')
+    username = request.form.get('username')
+    password = request.form.get('password')
 
     user = dao.auth_user(username=username, password=password)
-
     if user:
         login_user(user=user)
 
     return redirect('/admin')
 
 
+@login.user_loader
+def load_user(user_id):
+    return dao.get_user_by_id(user_id)
+
+
 if __name__ == '__main__':
     from app import admin
     app.run(debug=True)
-
-
-
